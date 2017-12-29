232ffd87de9f06d739d1d68d9719d501d3215a04
Suliac LE-GUILLOU <sle-guil@e2r10p9.42.fr>
2017-12-29 16:23:47 +0100

    init rust/rocket stack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # webservice-emote-mattermost
 Service web permettant de taper des commandes pour rajouter des emotes à la fin des phrases à la manière de la commande native /shrug de Mattermost.
+
+Implementation en rust en cours.
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+#[cfg(test)] mod tests;
+
+use rocket_contrib::Template;
+use rocket_contrib::{Json, JsonValue};
+use rocket::State;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+type ID = usize;
+
+#[derive(Serialize, Deserialize)]
+struct MattemostSlashcommandRequest {
+    id: Option<ID>,
+    response_type: String,
+    text: String
+}
+
+#[get("/shrug")]
+fn shrug() -> JsonValue {
+    json!({
+        "response_type": "in_channel",
+        "text": format!("{} ¯\\_(ツ)_/¯", "message")
+    })
+}
+
+fn main() {
+	rocket::ignite().mount("/api", routes![shrug]).launch();
+}
