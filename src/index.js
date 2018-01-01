848c0ad165247966e542a889306d9e621fa5f9d3
sle-guil <sle-guil@student.42.fr>
2018-01-01 15:34:27 +0100

    add listing of emojo and server auth in add

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let port = 8000;
+var addemojo_token = '';
 
 /**
  * Init database and prepare the request
@@ -59,15 +60,39 @@ app.post('/api/emojo', function (req, res) {
  */
 app.post('/api/emojo/add', function(req, res) {
 	// Need to check the server key
-	let insert_emojo = db.prepare("INSERT OR IGNORE INTO emojo VALUES ((?), (?))");
+	if (req.body.token === addemojo_token) {
+		let insert_emojo = db.prepare("INSERT OR IGNORE INTO emojo VALUES ((?), (?))");
 
-	console.log("Adding " + req.body.command + " " + req.body.text);
-	insert_emojo.run(req.body.command, req.body.text);
-	insert_emojo.finalize();
+		console.log("Adding " + req.body.command + " " + req.body.text);
 
-	res.send({
-		response_type: 'ephemeral',
-		text: 'Emojo inserted, add the corresponding slash command to enable it'
+		console.log(req.body);
+		insert_emojo.run(req.body.command, req.body.text);
+		insert_emojo.finalize();
+
+		res.send({
+			response_type: 'ephemeral',
+			text: 'Emojo inserted, add the corresponding slash command to enable it'
+		});
+	} else {
+		res.send({
+			response_type: 'error',
+			text: 'You do not have correct rigth for this'
+		})
+	}
+});
+
+app.get('/api/emojo/list', function(req, res) {
+	let list_emojo = db.prepare("SELECT * FROM emojo");
+	let textList = "command emojo\n";
+
+	list_emojo.all(function(err, rows) {
+		rows.forEach(function(row) {
+			textList = textList + row.name + " " + row.emojo + "\n";
+		});
+		res.send({
+			response_type: 'ephemeral',
+			text: textList
+		});
 	});
 });
 
